f0718f845409866d3a9b7930148823c374b18985
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-09-04 16:39:42 -0300

    final

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,10 @@
 
-$(function () {
-    $("#p-1-placeholder").load("../cards/card-1/p-1.html");
-});
+// $(function () {
+//     $("#p-1-placeholder").load("../cards/card-1/p-1.html");
+// });
+
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+input.addEventListener('keyup', function() {
+    h1.innerHTML = input.value;
+})
